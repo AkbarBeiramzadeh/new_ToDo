b37e8fef5c7f2d062dcf8d1a03384721ab3bfef9
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-23 15:01:01 +0330

    [ADD] path api-auth

--- core/core/urls.py
+++ core/core/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls', namespace='todo')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
 
 if settings.DEBUG:
--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -2,14 +2,12 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
 from .serializers import TaskSerializer
 from ...models import Task
-from .permissions import IsOwner
 
 
 class TaskModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
 
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
-
