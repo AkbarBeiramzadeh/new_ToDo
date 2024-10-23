9d7bda3da945daab4ca3270039b7416199e284b6
beyramzadeh <akbarbareghe7@gmail.com>
2024-10-23 14:55:43 +0330

    [ADD] show tasks only for owner

--- core/todo/api/v1/serializers.py
+++ core/todo/api/v1/serializers.py
@@ -6,3 +6,15 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ('id', 'user', 'title', 'state', 'created_at', 'updated_at')
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet', None)
+            rep.pop('relative_url', None)
+            rep.pop('absolute_url', None)
+        else:
+            rep.pop('content', None)
+        # if request.parser_context.get('request').user == instance.user:
+        return rep
--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -2,9 +2,14 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
 from .serializers import TaskSerializer
 from ...models import Task
+from .permissions import IsOwner
 
 
 class TaskModelViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
