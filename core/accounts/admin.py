5e3feb84072f626c4269009bbbc2187b5f6523fc
akbar <akbarbareghe7@gmail.com>
2024-11-01 09:54:02 -0800

    [ADD] CustomUserCreationForm

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django import forms
 
 User = get_user_model()
 
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='password',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='confirm password',widget=forms.PasswordInput)
 
 class CustomUserAdmin(UserAdmin):
     model = User
+    add_form = CustomUserCreationForm
     list_display = ('email', 'is_superuser', 'is_active')
     list_filter = ('email', 'is_superuser', 'is_active')
     search_fields = ('email',)
