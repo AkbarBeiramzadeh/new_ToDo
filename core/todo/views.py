f822f8897790c39d30e68e89c339955f01ff7ccf
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-09 15:32:28 +0330

    [UPD] reformat flake8

--- core/todo/views.py
+++ core/todo/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
-    TemplateView,
     DeleteView,
     UpdateView,
     CreateView,
