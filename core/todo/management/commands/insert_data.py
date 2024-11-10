ca3f1772b2790a1c773de9a72553ed6c77bbf941
beyramzadeh <akbarbareghe7@gmail.com>
2024-11-10 11:18:27 +0330

    insert fake data

---
+++ core/todo/management/commands/insert_data.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from accounts.models import User
+from todo.models import Task
+import random
+
+state_list = [
+    "ToDo",
+    "Done",
+    "InProgress",
+]
+
+
+class Command(BaseCommand):
+    help = "inserting dummy data"
+
+    def __init__(self, *args, **kwargs):
+        super(BaseCommand, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        # user = User.objects.get(email="admin@admin.com")
+        user = User.objects.create_user(email=self.fake.email(), password="Test@123456")
+        for _ in range(10):
+            Task.objects.create(user=user,
+                                title=self.fake.paragraph().split(" ")[0],
+                                state=random.choice(state_list)
+                                )
